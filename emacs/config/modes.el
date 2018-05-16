9193e83c2683f26f7d64a71c2f9212929a80c40b
Oliver Strickson <ostrickson@turing.ac.uk>
2018-05-16 11:36:43 +0100

    auto-commit org-mode files

--- emacs/config/modes.el
+++ emacs/config/modes.el
@@ -16,3 +16,22 @@
   (ansi-color-apply-on-region (point-min) (point-max))
   (toggle-read-only))
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+(add-hook 'tex-mode-hook 'ezbf-minor-mode)
+
+
+(require 'vc)
+(add-hook 'after-save-hook
+	  (lambda ()
+	    "Commit the file to version control when the buffer is in org-mode"
+	    (let ((fname (buffer-file-name)))
+	      (when (and (eq major-mode 'org-mode)
+			 (vc-registered fname))
+		(vc-checkin (list fname)
+			    (vc-backend fname)
+			    (concat "Update " (file-name-nondirectory fname)))))))
+
+(defun org-files-current-directory ()
+    (directory-files default-directory t ".*.org"))
+
+(setq org-refile-targets '((nil :maxlevel . 9) (org-files-current-directory :maxlevel . 9)))
